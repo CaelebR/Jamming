b29a88451d87aa3c721b4c72669cf3982d5b5b1f
CaelebR <caelebrenner@gmail.com>
2025-08-19 15:01:59 -0600

    Remove <filename>

--- src/SpotifyAPIInfo.jsx
+++
@@ -1,6 +0,0 @@
-
-// SpotifyAPIInfo.jsx
-export const ClientID = '3dec977e552f43aa863eda56ff15bec3';
-export const RedirectURI = 'http://127.0.0.1:5173/'; // or /callback
-
-// This file contains sensitive information and should not be shared publicly.
